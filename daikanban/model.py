6a5ec17276d77f4a5809837b47a9863948236c54
Jeremy Silver <jeremys@nessiness.com>
2024-02-29 19:44:58 -0500

    refactor: switch from pydantic dataclass to BaseModel for richer API

--- daikanban/model.py
+++ daikanban/model.py
@@ -1,12 +1,10 @@
 import base64
 from collections.abc import Collection
-from dataclasses import replace
 from datetime import datetime, timedelta
 from typing import Annotated, Any, Literal, Optional, TypeAlias, TypeVar
 from uuid import uuid4
 
-from pydantic import AnyUrl, PlainSerializer, RootModel, computed_field, model_validator
-from pydantic.dataclasses import Field, dataclass
+from pydantic import AnyUrl, BaseModel, Field, PlainSerializer, computed_field, model_validator
 
 
 T = TypeVar('T')
@@ -14,6 +12,7 @@ T = TypeVar('T')
 Id: TypeAlias = Annotated[str, Field(min_length=12, max_length=12)]
 TaskStatus = Literal['pending', 'active', 'paused', 'complete']
 Datetime = Annotated[datetime, PlainSerializer(lambda dt: dt.isoformat()[:19], return_type=str)]
+Timedelta = Annotated[timedelta, PlainSerializer(lambda td: td.total_seconds(), return_type=float)]
 
 
 def _get_random_id() -> Id:
@@ -44,24 +43,13 @@ class TaskStatusError(KanbanError):
     """Error that occurs when a task's status is invalid for a certain operation."""
 
 
-class JSONModel:
-    """Base class for Pydantic dataclasses that can serialize themselves to JSON."""
-
-    def _as_root_model(self: T) -> RootModel[T]:
-        """Gets this object as a RootModel instance."""
-        return RootModel[type(self)](self)  # type: ignore
-
-    def to_dict(self, **kwargs: Any) -> dict[str, Any]:
-        """Converts an object to a dict via pydantic RootModel."""
-        return self._as_root_model().model_dump(**kwargs)  # type: ignore
-
-    def to_json_string(self, **kwargs: Any) -> str:
-        """Converts an object to a JSON string via pydantic RootModel."""
-        return self._as_root_model().model_dump_json(**kwargs)
+class Model(BaseModel):
+    """Base class setting up pydantic configs."""
+    class Config:  # noqa: D106
+        frozen = True
 
 
-@dataclass(frozen=True)
-class Project(JSONModel):
+class Project(Model):
     """A project associated with multiple tasks."""
     name: str = Field(
         description='Project name'
@@ -80,8 +68,7 @@ class Project(JSONModel):
     )
 
 
-@dataclass(frozen=True)
-class Task(JSONModel):
+class Task(Model):
     """A task to be performed."""
     name: str = Field(
         description='Task name'
@@ -127,7 +114,7 @@ class Task(JSONModel):
         default=None,
         description='Time the task was completed'
     )
-    prior_time_worked: Optional[timedelta] = Field(
+    prior_time_worked: Optional[Timedelta] = Field(
         default=None,
         description='Total time the task was worked on prior to last_started_time'
     )
@@ -156,7 +143,7 @@ class Task(JSONModel):
 
     @computed_field  # type: ignore
     @property
-    def total_time_worked(self) -> timedelta:
+    def total_time_worked(self) -> Timedelta:
         """Gets the total time worked on the task."""
         td = timedelta(0) if (self.prior_time_worked is None) else self.prior_time_worked
         if self.last_started_time is not None:
@@ -184,33 +171,33 @@ class Task(JSONModel):
         Otherwise raises a TaskStatusError."""
         if self.status == 'pending':
             now = datetime.now()
-            return replace(self, first_started_time=now, last_started_time=now)  # type: ignore
+            update = {'first_started_time': now, 'last_started_time': now}
+            return self.model_copy(update=update)
         raise TaskStatusError(f'cannot start Task with status {self.status!r}')
 
     def completed(self) -> 'Task':
         """Returns a new completed version of the Task, if its status is active.
         Otherwise raises a TaskStatusError."""
         if self.status == 'active':
-            return replace(self, completed_time=datetime.now())  # type: ignore
+            return self.model_copy(update={'completed_time': datetime.now()})
         raise TaskStatusError(f'cannot complete Task with status {self.status!r}')
 
     def paused(self) -> 'Task':
         """Returns a new paused version of the Task, if its status is active.
         Otherwise raises a TaskStatusError."""
         if self.status == 'active':
-            return replace(self, last_started_time=None, prior_time_worked=self.total_time_worked)  # type: ignore
+            return self.model_copy(update={'last_started_time': None, 'prior_time_worked': self.total_time_worked})
         raise TaskStatusError(f'cannot pause Task with status {self.status!r}')
 
     def restarted(self) -> 'Task':
         """Returns a new restarted version of the Task, if its status is paused.
         Otherwise raises a TaskStatusError."""
         if self.status == 'paused':
-            return replace(self, last_started_time=datetime.now())  # type: ignore
+            return self.model_copy(update={'last_started_time': datetime.now()})
         raise TaskStatusError(f'cannot restart Task with status {self.status!r}')
 
 
-@dataclass(frozen=True)
-class DaiKanban(JSONModel):
+class DaiKanban(Model):
     """A database of projects and tasks."""
     projects: dict[Id, Project]
     tasks: dict[Id, Task]
@@ -235,7 +222,7 @@ class DaiKanban(JSONModel):
     def update_project(self, project_id: Id, **kwargs: Any) -> None:
         """Updates a project with the given keyword arguments."""
         proj = self.get_project(project_id)
-        self.projects[project_id] = replace(proj, **kwargs)  # type: ignore
+        self.projects[project_id] = proj.model_copy(update=kwargs)
 
     def delete_project(self, project_id: Id) -> None:
         """Deletes a project with the given ID."""
@@ -260,7 +247,7 @@ class DaiKanban(JSONModel):
     def update_task(self, task_id: Id, **kwargs: Any) -> None:
         """Updates a task with the given keyword arguments."""
         task = self.get_task(task_id)
-        self.tasks[task_id] = replace(task, **kwargs)  # type: ignore
+        self.tasks[task_id] = task.model_copy(update=kwargs)
 
     def delete_task(self, task_id: Id) -> None:
         """Deletes a task with the given ID."""
