3f3ec6be4cea0c4796937e74db699a78a8129fc0
Jeremy Silver <jeremys@nessiness.com>
2024-02-29 20:06:31 -0500

    test: simple unit test for serialize/deserialize

---
+++ tests/test_model.py
@@ -0,0 +1,12 @@
+from daikanban.model import DaiKanban, Project, Task, get_new_id
+
+
+def test_serialization():
+    proj = Project(name='myproj')
+    proj_id = get_new_id()
+    task = Task(name='mytask')
+    task_id = get_new_id()
+    dk = DaiKanban(projects={proj_id: proj}, tasks={task_id: task})
+    for obj in [proj, task, dk]:
+        d = obj.model_dump()
+        assert type(obj)(**d).model_dump() == d
