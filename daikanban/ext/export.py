048cb6e45d37b28d12696d27ef49b3d1103d90de
Jeremy Silver <jeremys@nessiness.com>
2024-08-22 16:08:07 -0400

    feat: more abstraction in daikanban.ext.export

--- daikanban/ext/export.py
+++ daikanban/ext/export.py
@@ -1,51 +1,75 @@
 from abc import ABC, abstractmethod
 from collections.abc import Iterable
 import json
-from typing import IO, Any, Generic, TypeVar
+from pathlib import Path
+from typing import IO, Any, Generic, TypeVar, Union
 
-from daikanban.task import Task
+from daikanban.model import Board
 
 
 T = TypeVar('T')
 T_IO = TypeVar('T_IO')
 
+AnyPath = Union[str, Path]
 
-JSONDict = dict[str, Any]
 
-
-class BaseExporter(ABC, Generic[T, T_IO]):
-    """Base class for exporting Tasks from daikanban to some file format."""
-
-    @abstractmethod
-    def convert_task(self, task: Task) -> T:
-        """Converts a Task to the associated type."""
+class FileWritable(ABC, Generic[T_IO]):
+    """Base class for something that can be serialized to a file."""
 
     @abstractmethod
-    def save_tasks(self, tasks: Iterable[Task], fp: T_IO, **kwargs: Any) -> None:
-        """Saves Tasks to a file-like object."""
+    def write(self, fp: T_IO, **kwargs: Any) -> None:
+        """Writes to a file-like object."""
 
 
-class JSONDictExportable(ABC):
-    """Base class for an object that can be converted to a JSON dict."""
+FW = TypeVar('FW', bound=FileWritable)
+
+
+class JSONExportable(ABC):
+    """Base class for an object that can be converted to a JSON object."""
 
     @abstractmethod
-    def to_json_dict(self) -> JSONDict:
-        """Converts the object to a JSON-serializable dict."""
+    def to_json_obj(self) -> Any:
+        """Converts the object to a JSON-serializable object."""
 
 
-J = TypeVar('J', bound=JSONDictExportable)
+class JSONWritable(JSONExportable, FileWritable[IO[str]]):
+    """Base class for something that can write to a JSON file."""
+
+    def write(self, fp: IO[str], **kwargs: Any) -> None:
+        """Writes to a JSON file."""
+        json.dump(self.to_json_obj(), fp, **kwargs)
+
+
+class JSONLinesWritable(JSONExportable, FileWritable[IO[str]]):
+    """Base class for something that can write to a JSONL (JSON lines) file."""
+
+    def write(self, fp: IO[str], **kwargs: Any) -> None:
+        """Writes to a JSON lines file."""
+        obj = self.to_json_obj()
+        assert isinstance(obj, Iterable)
+        for val in obj:
+            fp.write(json.dumps(val, indent=None, **kwargs))
+            fp.write('\n')
+
+
+class BaseExporter(ABC, Generic[FW, T_IO]):
+    """Base class for exporting a daikanban Board to some external file format."""
+
+    @abstractmethod
+    def convert_board(self, board: Board) -> FW:
+        """Converts a Board to the associated type."""
+
+    def export_board(self, board: Board, fp: T_IO, **kwargs: Any) -> None:
+        """Exports a Board to a file."""
+        self.convert_board(board).write(fp, **kwargs)
+
+
+J = TypeVar('J', bound=JSONWritable)
 
 
 class JSONExporter(BaseExporter[J, IO[str]]):
-    """Base class for exporting Tasks to JSON."""
+    """Base class for exporting a Board to a JSON file."""
 
-    def save_tasks_to_json(self, tasks: Iterable[Task], fp: IO[str], **kwargs: Any) -> None:
-        """Saves the tasks to a JSON file (list of dicts)."""
-        objs = [self.convert_task(task).to_json_dict() for task in tasks]
-        json.dump(objs, fp, **kwargs)
 
-    def save_tasks_to_jsonl(self, tasks: Iterable[Task], fp: IO[str], **kwargs: Any) -> None:
-        """Saves the tasks to a JSONL file (newline-separated dicts)."""
-        for task in tasks:
-            fp.write(json.dumps(self.convert_task(task).to_json_dict(), indent=None, **kwargs))
-            fp.write('\n')
+class JSONLinesExporter(BaseExporter[J, IO[str]]):
+    """Base class for exporting a Board to a JSONL (JSON lines) file."""
